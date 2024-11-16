f0217c639bf6a7e5e329ff5e28e0265313f1e8f8
Liam White <liamawhite@gmail.com>
2024-11-16 10:20:19 -0800

    Use licenser nix flake

--- flake.nix
+++ flake.nix
@@ -1,27 +1,27 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    utils.url = "github:numtide/flake-utils";
+    licenser = { url = "github:liamawhite/licenser/bdf2c1beeaf09aae9f27b9d680b5b6aa22e4f1a0"; inputs.nixpkgs.follows = "nixpkgs"; };
   };
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        licenser = import ./nix/packages/licenser.nix { inherit pkgs; };
-      in
-      with pkgs;
-      {
-        devShells.default = mkShell {
-          buildInputs = [
-            git
-            go
-            ffmpeg
-            ttyd
-            vhs
-            licenser
-          ];
-        };
-      }
-    );
+  outputs = { self, nixpkgs, utils, licenser }:
+    utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.git
+              pkgs.go
+              pkgs.ffmpeg
+              pkgs.ttyd
+              pkgs.vhs
+              licenser.packages.${system}.default
+            ];
+          };
+        }
+      );
 }
 
